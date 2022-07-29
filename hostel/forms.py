7b1e60e9729b457ca47884644b7d9040b1ec6667
Moadams <adamsmichael46@gmail.com>
2022-07-29 01:23:54 -0700

    handled login error

--- hostel/forms.py
+++ hostel/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.contrib import admin
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 
 
 # Creating a BaseForm that assigns all the fields in the form to the form-control class
@@ -17,16 +19,34 @@ class BaseForm(forms.Form):
 
 
 # Creating a Form used to create a new user
+# class CreateUserForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = [
+#             'username',
+#             'first_name',
+#             'last_name',
+#             'password1',
+#             'password2'
+#         ]
+
 class CreateUserForm(UserCreationForm):
-    class Meta:
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = [
-            'username',
-            'first_name',
-            'last_name',
-            'password1',
-            'password2'
-        ]
+
+
+class MyUserAdmin(UserAdmin):
+    add_form = CreateUserForm
+
+# admin.site.register(User,MyUserAdmin)   
 
 # Creating a form to add a new worker
 class WorkerForm(BaseForm,ModelForm):
