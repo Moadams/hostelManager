232aaaceac865abe396f82e431a7a6fa5c73c626
Moadams <adamsmichael46@gmail.com>
2022-08-01 02:47:42 -0700

    added form classes

--- hostel/forms.py
+++ hostel/forms.py
@@ -49,38 +49,38 @@ class CreateUserForm(UserCreationForm):
 # admin.site.register(User,MyUserAdmin)   
 
 # Creating a form to add a new worker
-# class WorkerForm(BaseForm,ModelForm):
-#     class Meta:
-#         model = Worker
-#         fields = [
-#             'name',
-#             'position',
-#             'salary'
-#         ]
+class WorkerForm(BaseForm,ModelForm):
+    class Meta:
+        model = Worker
+        fields = [
+            'name',
+            'position',
+            'salary'
+        ]
 
 # # Creating a form to add a new occupant
-# class OccupantForm(BaseForm,ModelForm):
-#     class Meta:
-#         model = Occupant
-#         fields = [
-#             'first_name',
-#             'last_name',
-#             'student_number',
-#             'phone',
-#             'email',
-#             'room_occupied',
-#             'amount_paid'
-#         ]
+class OccupantForm(BaseForm,ModelForm):
+    class Meta:
+        model = Occupant
+        fields = [
+            'first_name',
+            'last_name',
+            'student_number',
+            'phone',
+            'email',
+            'room_occupied',
+            'amount_paid'
+        ]
 
 
 # Creating a form to add a new room
-# class RoomForm(BaseForm,ModelForm):
-#     class Meta:
-#         model = Room
-#         fields = [
-#             'room_number',
-#             'room_type',
-#             'block',
-#             'spaces_available',
-#             'price_per_bed'
-#         ]
+class RoomForm(BaseForm,ModelForm):
+    class Meta:
+        model = Room
+        fields = [
+            'room_number',
+            'room_type',
+            'block',
+            'spaces_available',
+            'price_per_bed'
+        ]
